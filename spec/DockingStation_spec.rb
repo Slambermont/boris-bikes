fcf5d78912c08115749ad17354f63f70b2444b40
benfurber <ben@furber-consultancy.com>
2018-04-17 16:29:35 +0100

    Working release bike feature (with test)

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,4 +2,10 @@ require 'DockingStation'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases a working bike' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
 end
